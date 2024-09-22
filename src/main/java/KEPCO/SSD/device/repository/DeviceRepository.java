28639a9b6600b6875a987828266b73eb2e006693
Jeongyeon_Song <songj239587@gmail.com>
2024-09-22 14:21:58 +0900

    Fix_userId data type

--- src/main/java/KEPCO/SSD/device/repository/DeviceRepository.java
+++ src/main/java/KEPCO/SSD/device/repository/DeviceRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface DeviceRepository extends JpaRepository<Device, String> {
-    List<Device> findByUserId(int userId); // int로 변경
-    Optional<Device> findByUserIdAndSerialNumber(int userId, String serialNumber); // int로 변경
-    void deleteByUserIdAndSerialNumber(int userId, String serialNumber); // int로 변경
+    List<Device> findByUserId(long userId);
+    Optional<Device> findByUserIdAndSerialNumber(long userId, String serialNumber);
+    void deleteByUserIdAndSerialNumber(long userId, String serialNumber);
 }
