ae404c1a3906408e4dd026213014920496091c46
snowykte0426 <snowykte0426@naver.com>
2024-09-27 22:10:01 +0900

    Fix_Modifying to REST domain design rules

--- src/main/java/KEPCO/SSD/user/controller/UserController.java
+++ src/main/java/KEPCO/SSD/user/controller/UserController.java
@@ -24,7 +24,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @PostMapping("/send_code")
+    @PostMapping("/send-code")
     public ResponseEntity<String> sendCode(@RequestBody @Valid SendCodeRequestDto request) {
         userService.sendVerificationCode(request.getPhoneNumber());
         return ResponseEntity.ok("인증메세지 발송");
